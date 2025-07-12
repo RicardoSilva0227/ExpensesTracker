08f27587d4eebfeae22576c7e1c00f403478ce9d
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2025-07-12 15:42:23 +0100

    dbcontext

---
+++ ExpenseTrackerAPI/Models/Configs.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTrackerAPI.Models
+{
+    public class Configs
+    {
+        /// <summary>
+        /// Id of the expense
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        /// <summary>
+        /// Id of the currency
+        /// </summary>
+        public int CurrencyId { get; set; }
+        /// <summary>
+        /// location path for the invoice storage
+        /// </summary>
+        public string InvoicePath { get; set; }
+
+    }
+}
